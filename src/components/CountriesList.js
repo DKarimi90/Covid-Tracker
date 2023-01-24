a3eccc338ce633f0727e267c9adc9083321fb690
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-01-24 14:19:15 +0300

    Initial setup

---
+++ src/components/CountriesList.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+function CountriesList() {
+const [countries, setCountries] = useState([])
+useEffect(()=>{
+fetch(`https://api.covid19api.com/countries`)
+.then(res => res.json())
+.then(countries => setCountries(countries))
+}, [])
+console.log(countries)
+const countryList = countries.map((country)=>(
+    <li>{country.Country}</li>
+))
+return (
+        // function to fetch data
+       <div>
+        <ul>
+         {countryList}
+        </ul>
+       </div>
+    )
+}
+export default CountriesList
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <div className="nav">
+            <a href="#">Home</a>
+            <a href="#">CountriesList</a>
+            <a href="#">LogIn</a>
+            <a href="#">LogOut</a>
+
+        </div>
+    )
+}
+
+export default NavBar;
