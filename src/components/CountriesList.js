9f9d3310a6d47021b8d9e9639f5ffe523436f9b1
david <davmwa90@gmail.com>
2023-01-26 14:15:11 +0300

    Edited my CountriesList code

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -9,10 +9,11 @@ function CountriesList() {
   // function to fetch and list the countries 
 
   useEffect(() => {
-    fetch(`https://api.covid19api.com/countries`)
+    fetch(`https://disease.sh/v3/covid-19/countries`)
     .then(res => res.json())
-    .then(countries => setCountries(countries))
+    .then(data => setCountries(data))
   }, [])
+  // console.log(countries)
 
   function handleSearch(e) {
     setSearchValue(e.target.value)
@@ -20,49 +21,50 @@ function CountriesList() {
 
 
 
+
 // function to display covid data for only selected country 
 
-  function onChange() {
-    fetch(`https://disease.sh/v3/covid-19/countries`)
-    .then(res => res.json())
-    .then(data=> {
-      setData(data)
-      console.log(data)
-      data.map(country => {
-        <div>{country.id} {country.critical} {country.recorded}</div>
-      })
-    })
-  }
- 
-
-
-
+  // useEffect(() => {
+  //   fetch(`https://disease.sh/v3/covid-19/countries`)
+  //   .then(res => res.json())
+  //   .then(data=> {
+  //     setData(data)
+  //     console.log(data)
+  //     data.map(country => <div> 
+  //       {/* {country.country}  */}
+  //       {country.active} 
+  //       {country.cases}</div>
+  //     )
+  //   })
+  // }, []); 
 
 
   const filteredCountries = countries.filter(country => {
-    return country.Country.toLowerCase().includes(searchValue.toLowerCase())
+    return country.country.toLowerCase().includes(searchValue.toLowerCase())
   })
 
   const countryList = filteredCountries.map((country, index) => (
-    <li key={index}>{country.Country}</li>
+    <div>
+      <h2 key={index}>{country.country}</h2>
+    </div>
   ))
 
   return (
     <div>
       <h3>list of countries</h3>
       <input className="search" type="text" value={searchValue} onChange={handleSearch} placeholder="Search for a country" />
-
       <ul>
         {countryList}
       </ul>
-      {data && (
-    <div>
+ 
+  
+    {/* <div>
       <h3>Selected country data</h3>
       <p>Country: {data.country}</p>
       <p>Critical cases: {data.critical}</p>
       <p>Recorded cases: {data.cases}</p>
     </div>
-  )}
+   */}
 
     </div>
   )
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,10 +1,71 @@
+import React, {useEffect} from 'react'
+import {useState} from "react"
 
-function LogIn() {
+function CountriesList() {
+  const [countries, setCountries] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [data, setData] = useState([])
+
+  // function to fetch and list the countries 
+
+  useEffect(() => {
+    fetch(`https://disease.sh/v3/covid-19/countries`)
+    .then(res => res.json())
+    .then(countries => setCountries(countries))
+  }, [])
+
+  function handleSearch(e) {
+    setSearchValue(e.target.value)
+  }
+
+
+
+// function to display covid data for only selected country 
+
+  function onChange() {
+    fetch(`https://disease.sh/v3/covid-19/countries`)
+    .then(res => res.json())
+    .then(data=> {
+      setData(data)
+      console.log(data)
+      data.map(country => {
+        <div>{country.id} {country.critical} {country.recorded}</div>
+      })
+    })
+  }
+ 
+
+
+
+
+
+  const filteredCountries = countries.filter(country => {
+    return country.Country.toLowerCase().includes(searchValue.toLowerCase())
+  })
+
+  const countryList = filteredCountries.map((country, index) => (
+    <li key={index}>{country.Country}</li>
+  ))
+
   return (
     <div>
-    
+      <h3>list of countries</h3>
+      <input className="search" type="text" value={searchValue} onChange={handleSearch} placeholder="Search for a country" />
+
+      <ul>
+        {countryList}
+      </ul>
+      {data && (
+    <div>
+      <h3>Selected country data</h3>
+      <p>Country: {data.country}</p>
+      <p>Critical cases: {data.critical}</p>
+      <p>Recorded cases: {data.cases}</p>
+    </div>
+  )}
+
     </div>
   )
 }
 
-export default LogIn
+export default CountriesList; 
