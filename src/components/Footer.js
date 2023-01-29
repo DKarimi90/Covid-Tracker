ccb39e5c50e1fe202a5ad53b52bb7307e436c031
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-01-29 12:05:18 +0300

    added new components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <strong>© copyright 2023. Covid-19 Tracker</strong>
+         <div><small>mark.mutuku@student.moringaschool.com,ian.munene@student.moringaschool.com,
+            david.mwai@student.moringaschool.com, cynthia.jepkosgei@student.moringaschool.com </small></div>
+        </div>
+    )
+}
+
+export default Footer
